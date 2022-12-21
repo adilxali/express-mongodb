7dcca29ce9691575018073a22699f2a476ce2880
Adil Ali <aadilali444@.com>
2022-12-21 23:06:01 +0530

    api

--- index.js
+++ index.js
@@ -134,5 +134,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
